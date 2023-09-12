e592b9668507e3908e49f859e6b0aee88b445f21
macyava <674631003@qq.com>
2023-09-12 18:20:35 +0800

    second version

---
+++ Student.java
@@ -0,0 +1,34 @@
+package com.hspedu.extends_;
+
+public class Student extends Person {
+    private String id;
+    private int score;
+
+
+    public Student(String name, int age, String id, int score) {
+        super(name, age);
+        this.id = id;
+        this.score = score;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+    public String say() {
+        return super.say() + " My id is " + id + ". My score is " + score + ".";
+    }
+}
